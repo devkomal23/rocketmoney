d44dff5d9a3b86b974690eefab53eaf122db28ef
devkomal23 <devshuklakomal@gmail.com>
2026-06-12 13:40:09 +0530

    deply vercel

--- public/index.php
+++ public/index.php
@@ -4,17 +4,18 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-$_ENV['APP_STORAGE'] = '/tmp';
 
+// Force storage to /tmp
+$_ENV['APP_STORAGE'] = '/tmp';
+putenv('APP_STORAGE=/tmp');
+
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// 1. Boot the application
+// Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// 2. IMPORTANT: Register core Service Providers manually if they aren't loading
-$app->register(Illuminate\View\ViewServiceProvider::class);
-$app->register(Illuminate\Routing\RoutingServiceProvider::class);
-
+// Run the kernel
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
