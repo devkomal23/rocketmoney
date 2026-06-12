7f16751b561e0e02c281b155e424adadee55df18
devkomal23 <devshuklakomal@gmail.com>
2026-06-12 13:03:50 +0530

    deply vercel

--- public/index.php
+++ public/index.php
@@ -1,20 +1,26 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+// Set storage and cache to writable /tmp directory
+$_ENV['APP_STORAGE'] = '/tmp';
 
-// Register the Composer autoloader...
+// Load the autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
+$kernel = $app->make(Kernel::class);
 
-$app->handleRequest(Request::capture());
+// Handle the request
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
+// REMOVED the extra handleRequest call
