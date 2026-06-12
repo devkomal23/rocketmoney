1cad1657f2e4a4db02218b33f3fce0cff7d11f92
devkomal23 <devshuklakomal@gmail.com>
2026-06-12 13:49:56 +0530

    deply vercel

--- public/index.php
+++ public/index.php
@@ -2,20 +2,23 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
 
 define('LARAVEL_START', microtime(true));
 
-// Force storage to /tmp
-$_ENV['APP_STORAGE'] = '/tmp';
-putenv('APP_STORAGE=/tmp');
-
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// Create the application
+$app = new Application(dirname(__DIR__));
 
-// Run the kernel
+// Manually bind the Kernel
+$app->singleton(Kernel::class, App\Http\Kernel::class);
+
+// CRITICAL: Manually register View and Routing providers
+$app->register(Illuminate\View\ViewServiceProvider::class);
+$app->register(Illuminate\Routing\RoutingServiceProvider::class);
+$app->register(Illuminate\Events\EventServiceProvider::class);
+
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
@@ -23,5 +26,4 @@ $response = $kernel->handle(
 );
 
 $response->send();
-
 $kernel->terminate($request, $response);
