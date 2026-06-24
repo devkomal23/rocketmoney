79d9685c99e900776337b98fb965f58534961795
devkomal23 <devshuklakomal@gmail.com>
2026-06-24 10:02:36 +0530

    home page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->statefulApi(); // Important if using Sanctum
         $middleware->validateCsrfTokens(except: [
             'api/webhooks/stripe', 
         ]);
