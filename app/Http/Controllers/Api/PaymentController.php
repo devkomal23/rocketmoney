6a37b5b4bce529b47fc78b5af7742e63d4007731
devkomal23 <devshuklakomal@gmail.com>
2026-06-26 17:13:23 +0530

    loan subscription

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -67,9 +67,9 @@ class PaymentController extends Controller
         ]);
 
         $response = Http::withHeaders([
-            'x-client-id'           => 'd335846e-4b96-44c3-912f-0e3801b2184d',
-            'x-client-secret'       => 'As6g2YXJRWVadS9XSlnPLm5sKKk3CIrJ',
-            'x-product-instance-id' => '8f8b1a79-682a-4740-a78b-e356fab9a59a',
+            'x-client-id'           => '292c6e76-dabf-49c4-8e48-90fba2916673',
+            'x-client-secret'       => '7IZMe9zvoBBuBukLiCP7n4KLwSOy11oP',
+            'x-product-instance-id' => '9480d765-ebaf-4061-91d4-66af89c3e434',
             'Accept'                => 'application/json', 
         ])->post('https://dg-sandbox.setu.co/api/verify/ban', [ 
             'accountNumber' => (string) $validated['accNo'],
@@ -103,12 +103,13 @@ class PaymentController extends Controller
         }
         
 
-return response()->json([
-    'success' => false,
-    'status' => $response->status(),
-    'setu_response' => $response->json(),
-    'raw_response' => $response->body(),
-], $response->status());    }
+        return response()->json([
+            'success' => false,
+            'status' => $response->status(),
+            'setu_response' => $response->json(),
+            'raw_response' => $response->body(),
+        ], $response->status());    
+    }
 
 
     public function createSubscription(Request $request)
