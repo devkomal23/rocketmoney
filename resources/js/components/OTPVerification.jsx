2801f78faee5183cbe6c209fd0a7c63760d79276
devkomal23 <devshuklakomal@gmail.com>
2026-07-01 15:33:18 +0530

    razorpay setup

--- resources/js/components/OTPVerification.jsx
+++ resources/js/components/OTPVerification.jsx
@@ -73,7 +73,7 @@ export default function OTPVerification() {
           const isFeePaid = is_fee_paid === true || assessment_fee_status === 'paid';
           const isKycVerified = kyc_status === 'verified';
           if (user.is_registration_complete != 1) {
-            navigate('/complete-application',{ state: { mobile: mobileNumber } });
+            navigate('/complete_application',{ state: { mobile: mobileNumber } });
           } 
           else if (kyc_status !== 'verified') {
             navigate('/verify-kyc');
