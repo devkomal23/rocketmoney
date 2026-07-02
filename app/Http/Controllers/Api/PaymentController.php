4374d69a21b61a45cf5d5c710863714d65204b30
devkomal23 <devshuklakomal@gmail.com>
2026-07-02 12:30:05 +0530

    razorpay setup

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -187,14 +187,14 @@ class PaymentController extends Controller
         return response()->json(['status' => 'ok']);
     }
 
-    public function testSetu()
+public function testSetu()
 {
     $response = Http::acceptJson()
         ->asJson()
         ->withHeaders([
-        'x-client-id' => '292c6e76-dabf-49c4-8e48-90fba2916673',
-        'x-client-secret' => '7IZMe9zvoBBuBukLiCP7n4KLwSOy11oP',
-        'x-product-instance-id' => '9480d765-ebaf-4061-91d4-66af89c3e434',
+            'x-client-id' => env('SETU_CLIENT_ID'),
+            'x-client-secret' => env('SETU_CLIENT_SECRET'),
+            'x-product-instance-id' => env('SETU_PRODUCT_INSTANCE_ID'),
         ])
         ->post('https://dg-sandbox.setu.co/api/verify/ban', [
             'accountNumber' => '1234567890',
@@ -203,8 +203,6 @@ class PaymentController extends Controller
 
     return response()->json([
         'status' => $response->status(),
-        'body' => $response->body(),
-        'headers' => $response->headers(),
+        'body' => $response->json(),
     ]);
-}
-}
+}}
